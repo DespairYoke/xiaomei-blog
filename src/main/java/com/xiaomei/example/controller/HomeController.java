6dae786d673b299a699bf164dd37b4de08c2d3d5
love <810095178@qq.com>
2018-09-13 23:23:08 +0800

    zwd

--- src/main/java/com/xiaomei/example/controller/HomeController.java
+++ src/main/java/com/xiaomei/example/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.xiaomei.example.controller;
 
 import com.xiaomei.example.domain.Article;
+import com.xiaomei.example.domain.Image;
 import com.xiaomei.example.service.ArticleService;
+import com.xiaomei.example.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.xml.ws.soap.Addressing;
 import java.util.List;
 
 /**
@@ -20,6 +23,9 @@ public class HomeController {
     @Autowired
     private ArticleService articleService;
 
+    @Autowired
+    private ImageService imageService;
+
     @RequestMapping("/")
     public String home(Model model) {
         List<Article> articles = articleService.selectAll();
@@ -27,9 +33,10 @@ public class HomeController {
         return "index";
     }
 
-    @RequestMapping("/picture")
-    public String picture() {
-
+    @RequestMapping("/share")
+    public String picture(Model model) {
+        List<Image> images = imageService.selectAll();
+        model.addAttribute("images",images);
         return "share";
     }
 }
